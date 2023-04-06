3b00e82d1b81db7d1eb3cf4514d0c26c196f6323
Wambani01 <georgewmbn@gmail.com>
2023-04-06 10:59:35 -0700

    using fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,7 +21,7 @@ def do_deploy(archive_path):
        remote_path = "/data/web_static/releases/{}/".format(no_extension)
        sym_link = "/data/web_static/current"
        put(archive_path, "/tmp/")
-       run("sudo mkdir - p {}".format(remote_path))
+       run("sudo mkdir -p {}".format(remote_path))
        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
        run("sudo rm /tmp/{}".format(compressed_file))
        run("sudo mv {}/web_static/* {}".format(remote_path))
