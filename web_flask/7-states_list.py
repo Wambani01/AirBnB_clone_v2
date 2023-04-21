d3b612c108e6b0b385a1e15486e53b2ac00608b2
Wambani01 <georgewmbn@gmail.com>
2023-04-21 19:51:03 +0000

    rendering html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def clean_up(exception=None):
-    """eliminates current Session"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """enumerate all states"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def clean_up(exception=None):
+    """eliminates current Session"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
