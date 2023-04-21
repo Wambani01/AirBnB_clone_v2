bacb2af2c729365ede23d189db038e236ce145a4
Wambani01 <georgewmbn@gmail.com>
2023-04-21 18:40:17 +0000

    fixed bugs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """a script to return all states from db"""
 
 from models import storage
@@ -11,16 +12,14 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def clean_up(exception=None):
     """a function to release db resources"""
-
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """a function to return all states from db"""
-
     states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
